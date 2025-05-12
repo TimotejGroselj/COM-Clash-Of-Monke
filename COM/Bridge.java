ad91ba37dc51d63e4a2f44e4840586c25dfde715
Timotej Groselj <timotej.groselj1@gmail.com>
2025-05-12 20:35:05 +0200

    fancy shit al neki

--- COM/Bridge.java
+++ COM/Bridge.java
@@ -1,8 +1,6 @@
 package COM;
 
 public class Bridge extends Troop {
-    private Vektor location;
-
     public Bridge(Vektor location) {
         super(null, 0, 0, 0, 0, 0, 0, 0, location, null, 0);
     }
