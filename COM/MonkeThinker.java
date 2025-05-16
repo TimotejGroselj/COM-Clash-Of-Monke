16f26ccb626b029a87774dfc3b8cd42be957aaca
Jauka <jaka.perbil@gmail.com>
2025-05-17 00:56:39 +0200

    choosedalokacija shoulda worka

--- COM/MonkeThinker.java
+++ COM/MonkeThinker.java
@@ -1,16 +1,43 @@
 package COM;
 
-import java.awt.*;
+
 import java.util.*;
 import java.util.List;
 
 public class MonkeThinker {
 
+    public Vektor Spawn;
+    public Troop Dude;
+    public int elixir;
+
     public MonkeThinker(Set<Troop> friendly,Set<Troop> enemy,int elixir) {
+        this.elixir = elixir;
+        int onfield = 0;
         for (Troop friend:friendly){
-            double x = friend.getLocation().getX();
-            double y = friend.getLocation().getY();
+            ChooseTheGuy BestMonke = new ChooseTheGuy(friend,enemy,this.elixir);
+            if (!friend.isOnFrendlyGround(MainLoop.HEIGHT)){
+                this.Dude = BestMonke.DoDefence();
+                this.Spawn = new ChooseTheLocation(this.Dude,friend).GetSpawn("Defence");
+            }
+            else {
+                if (friend.getCurrenthealth() > 100) { //tuki se to stotko nastimi, to je za tanke
+                    this.Dude = BestMonke.DoBackupDefence();
+                    this.Spawn = new ChooseTheLocation(this.Dude,friend).GetSpawn("BackupDefence");
+                }
+                else {
+                    this.Dude = BestMonke.DoOffence();
+                    this.Spawn = new ChooseTheLocation(this.Dude,friend).GetSpawn("Offence");
+                }
+            }
         }
+    }
+
+    public Troop getDude() {
+        return Dude;
+    }
+
+    public Vektor getSpawn() {
+        return Spawn;
     }
 }
 
@@ -39,17 +66,15 @@ class ChooseTheGuy{
         return null;
     }
 
-    public Troop DoOffense(){
+    public Troop DoOffence(){
         return null;
     }
 }
 
 class ChooseTheLocation {
     private Troop enemy;
-    private double a; //arena width
-    private double b; //arena height/2.0
-    private double ry; //left y cord
-    private double rx; //left x cord (
+    private double a = MainLoop.WIDTH;
+    private double ry = MainLoop.HEIGHT/2.0; //left y cord
     private Troop friend;
     private double range;
     private double x0;
@@ -63,21 +88,27 @@ class ChooseTheLocation {
     }
 
     public boolean IsInside(double x,double y){
-        return ((x >= rx || x <= rx+a)&&(y >= ry || y <= ry+b));
+        return ((x >= 0 && x <= a)&&(y >= ry/2 && y <= ry));
     }
 
     public Vektor GetSpawn(String strategy) {
         Random r = new Random();
         double x,y = 0.0;
-        if (Objects.equals(strategy, "Attack")) {
-            x = r.nextInt((int) rx, (int) (rx + a));
-            y = r.nextInt((int) ry, (int) (ry + b));
+        if (strategy.equals("Offence")) {
+            if (this.x0 < a/2) {
+                x = r.nextInt(0, (int) (a /2));
+                y = r.nextInt((int) (ry/2), (int) (ry));
+            }
+            else {
+                x = r.nextInt((int) (a / 2), (int) a);
+                y = r.nextInt((int) (2*ry/3), (int) (ry));
+            }
             return new Vektor(x, y);
-        } else if (Objects.equals(strategy, "Defence")) {
-            double parameter = 6.0;
+        } else if (strategy.equals("Defence")) {
+            double parameter = 6;
             double fi = Math.PI/parameter;
             List<Vektor> cords = new ArrayList<>();
-            for (int i = 0;i<(int)(parameter*2 + 1.0);i++){
+            for (int i = 0;i<(parameter*2 + 1);i++){
                 x = this.range*Math.sin(fi) + this.x0;
                 y = this.range*Math.cos(fi) + this.y0;
                 fi += Math.PI/parameter;
@@ -88,18 +119,26 @@ class ChooseTheLocation {
             int id = (int)(Math.random()* cords.size());
             return cords.get(id);
         }
-        return null;
+        else {
+            if (this.x0 < a/2) {
+                x = r.nextInt((int) (a / 3), (int) (a /2));
+                y = r.nextInt((int) (2*ry/3), (int) (ry));
+            }
+            else {
+                x = r.nextInt((int) (a / 2), (int) (2*a /3));
+                y = r.nextInt((int) (2*ry/3), (int) (ry));
+            }
+            return new Vektor(x, y);
+        }
     }
-
 }
 
 /*Plan za jutr:
-Dodj se emergency defence
-Pokomenteri rx,ry
-Pa une metode k so null
-Metodo dodefence preured za hashmap interactionov za dani troop
-U classu spawn nared zadevo da loh spawna uzad (torej ce vid tanka na drugi strani lahko sumi,da gre za push in more defendat, defenda doklr tank ne umre)
-Nadaljevanje od zgornga: ce ni tanka ampak je nek semi-tank lahko izbere a bo defendu al attacku based on threatning level pa elixir, ce je pa attack pa napad na drugi strani?
+Zbildi class za interactione med troopi (kdo koga zmaga glede na range: enemy range in pa friendly range)
+S tem zbuildi metode: BackupDefence bos iskou heavy hitterja al pa tanka,DoOffence zberes kergakol,DoDefence zberes najbolsga
+USE GLEDAS SE NA ELIXIR K JE NA VOLJO!!
+BackupDefence loh nardis da coresponda z lokacijo enemy tanka, pa da defenda doklr tank ne umre
+Kaj pa ni nobenega friendlyja na mapi kaj pa potem?
 Bottomline lots of work awaits you maggot
 */
 
