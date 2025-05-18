2ff178ad42a8602a7e9e24d54affa9ecdab825d3
Jauka <jaka.perbil@gmail.com>
2025-05-18 21:17:52 +0200

    ugabooga for now

--- COM/Start.java
+++ COM/Start.java
@@ -18,17 +18,50 @@ public class Start {
         start.setResizable(false);
         start.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Set layout
-        start.setLayout(new FlowLayout());
+        int width = 100;
+        int height = 100;
+        int space = 50;
+        int xt= 4*width + 3*space;
+        int rxt = (w-xt)/2;
+        int yt = 4*height + 3*space;
+        int ryt = (h-yt)/2;
 
-        // Create button
-        for (int i = 0;i<4;i++) {
-            JButton button = new JButton("+ "+i);
-            button.setPreferredSize(new Dimension(100, 40));
-            int CardNum = i;
-            button.addActionListener(e -> System.out.println("Presoo "+CardNum));
-            start.add(button);
+        for (int j = 0;j < 2;j++) {
+            for (int i = 0; i < 4; i++) {
+                ImageIcon icon = new ImageIcon("TeseterMonke.png");
+                JButton button = new JButton(icon);
+                int x = rxt + i * (width+space);
+                int y = ryt-150 + j * (height+space);
+                button.setBounds(x, y, width, height);
+                int CardNum = ((i + 1)+j*4);
+                button.addActionListener(e -> System.out.println("Presoo " + CardNum));
+                start.add(button);
+            }
         }
+        int yt2 = 9*height + 8*space;
+        int ryt2 = (h-yt2)/2;
+        int add =0;
+        for (int j = 0;j <4;j++) {
+            for (int i = 1; i < 5; i++) {
+                int CardNum = i + (8+j*4);
+                JButton button = new JButton("" + CardNum);
+                int x = i * (width + space);
+                int y = ryt2 + (height + space);
+                button.setBounds(x - 100, y + 400+ add, width, height);
+                button.addActionListener(e -> System.out.println("Presoo " + CardNum));
+                start.add(button);
+            }
+            add += 150;
+        }
+        start.setLayout(new BorderLayout());
+        JPanel panel = new JPanel();
+        JPanel north = new JPanel();
+        north.add(new JLabel("YOUR DECK"));
+        start.add(north,BorderLayout.NORTH);
+        panel.setBackground(Color.YELLOW);
+        panel.setBounds(0,0,w,h);
+        start.add(panel);
+
         start.setVisible(true);
     }
 }
