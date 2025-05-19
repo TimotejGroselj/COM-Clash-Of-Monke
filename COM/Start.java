2497bbf77eff643f51fc8e494c457989c250b347
Jauka <jaka.perbil@gmail.com>
2025-05-19 23:00:56 +0200

    oke but how do i access da setkus

--- COM/Start.java
+++ COM/Start.java
@@ -2,12 +2,15 @@ package COM;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.*;
+import java.util.List;
 
 public class Start {
     public static int h = MainLoop.HEIGHT;
     public static int w = MainLoop.WIDTH;
+    private static JButton first = null;
+
 
     public Start() {;
     }
@@ -26,15 +29,52 @@ public class Start {
         int yt = 4*height + 3*space;
         int ryt = (h-yt)/2;
 
+        Set<String> deck = new HashSet<>();
+        for (int i = 1;i < 9;i++){deck.add(Integer.toString(i));}
+        Set<String> rest = new HashSet<>();
+        for (int i = 9;i < 25;i++){rest.add(Integer.toString(i));}
+
+
+
+        ActionListener SwapCards = e -> {
+            JButton chosen = (JButton) e.getSource();
+            if (first == null) {
+                first = chosen;
+            } else {
+                String temp = first.getText();
+                String casu = chosen.getText();
+                if (first != chosen){
+                    if (deck.contains(temp) && rest.contains(casu)){
+                        deck.remove(temp);
+                        deck.add(casu);
+                        rest.remove(casu);
+                        rest.add(temp);
+                    }
+                    else {
+                        deck.remove(casu);
+                        deck.add(temp);
+                        rest.remove(temp);
+                        rest.add(casu);
+                    }
+                System.out.println(deck);
+                System.out.println(rest);
+                first.setText(chosen.getText());
+                chosen.setText(temp);
+                }
+                first = null;
+            }
+        };
+
+
         for (int j = 0;j < 2;j++) {
             for (int i = 0; i < 4; i++) {
-                ImageIcon icon = new ImageIcon("TeseterMonke.png");
-                JButton button = new JButton(icon);
+                JButton button = new JButton();
+                int CardNum = (i+1) + j*4;
+                button.setText(Integer.toString(CardNum));
                 int x = rxt + i * (width+space);
                 int y = ryt-150 + j * (height+space);
                 button.setBounds(x, y, width, height);
-                int CardNum = ((i + 1)+j*4);
-                button.addActionListener(e -> System.out.println("Presoo " + CardNum));
+                button.addActionListener(SwapCards);
                 start.add(button);
             }
         }
@@ -44,15 +84,18 @@ public class Start {
         for (int j = 0;j <4;j++) {
             for (int i = 1; i < 5; i++) {
                 int CardNum = i + (8+j*4);
-                JButton button = new JButton("" + CardNum);
+                JButton button = new JButton();
+                button.setText(Integer.toString(CardNum));
                 int x = i * (width + space);
                 int y = ryt2 + (height + space);
                 button.setBounds(x - 100, y + 400+ add, width, height);
-                button.addActionListener(e -> System.out.println("Presoo " + CardNum));
+                button.addActionListener(SwapCards);
                 start.add(button);
             }
             add += 150;
         }
+
+
         start.setLayout(new BorderLayout());
         JPanel panel = new JPanel();
         JPanel north = new JPanel();
@@ -61,7 +104,6 @@ public class Start {
         panel.setBackground(Color.YELLOW);
         panel.setBounds(0,0,w,h);
         start.add(panel);
-
         start.setVisible(true);
     }
 }
