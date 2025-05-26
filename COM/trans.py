06347e297fdedfc45bf5836eb3c3cd86e59be79e
Timotej <timotej.groselj1@gmail.com>
2025-05-26 09:27:18 +0200

    trans done

--- COM/trans.py
+++ COM/trans.py
@@ -2,6 +2,20 @@ import numpy as np
 import matplotlib.image as im
 import os
 
-os.
-img = im.imread('image.png')
-h, w, c = img.shape # višina, širina, 3 ali 4
+dirs = set(["animations", "pictures"])
+#direktori za obdelat
+for dir in dirs:
+    files = os.listdir(dir)
+    for file in files:
+        img = im.imread(dir+"\\"+file)
+        h, w, c = img.shape 
+        #če ima slika transparentnost vse pixle, ki niso dovolj "močni" naredimo nevidne
+        #ostale pa nastavimo da so polno transparentni
+        if c == 4:
+            for i in range(h):
+                for j in range(w):
+                    if img[i][j][3] <=0.3:
+                        img[i][j][3] = 0
+                    else:
+                        img[i][j][3] = 1
+        im.imsave(dir+"\\"+file ,img)
